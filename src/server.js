46f818a4e16f88cd2bb0cceda619a963f8ddbb48
molhamsheikh <molhamsa49@gmail.com>
2025-07-20 15:20:05 +0300

    add cors package

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8383;
 
-const authRoutes = require('./routes/auth');
-const adminRoutes = require('./routes/admin');
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
