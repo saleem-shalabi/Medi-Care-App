d80adaa1acf09dd1c0ac877361f96d484b2d99af
saleem-shalabi <92267347+saleem-shalabi@users.noreply.github.com>
2025-07-22 00:19:01 +0300

    Update (product) server.js

--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ const PORT = process.env.PORT || 8383;
 
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin");
+const productRoutes = require('./routes/product');
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
