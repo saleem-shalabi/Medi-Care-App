c0522b21fc54a8ea0037450112a87ce538c0a1e5
molhamsheikh <molhamsa49@gmail.com>
2025-07-23 16:15:07 +0300

    adit calidation product schemas

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,21 +1,24 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const productSchema = Joi.object({
-    nameEn: Joi.string().required(),
-    nameAr: Joi.string().required(),
-    company: Joi.string().required(),
-    category: Joi.string().required(),
-    description: Joi.string().optional(),
-    rate: Joi.number().min(0).max(5).optional(),
-    rentPrice: Joi.number().min(0).optional(),
-    sellPrice: Joi.number().min(0).optional(),
-    availableForRent: Joi.boolean().required(),
-    availableForSale: Joi.boolean().required(),
-    rentStock: Joi.number().integer().min(0).required(),
-    saleStock: Joi.number().integer().min(0).required(),
-    images: Joi.array().items(Joi.string().uri()).required(),
-    videos: Joi.array().items(Joi.string().uri()).optional(),
-    qrCode: Joi.string().uri().optional(),
+  nameEn: Joi.string().required(),
+  nameAr: Joi.string().required(),
+  companyEn: Joi.string().required(),
+  companyAr: Joi.string().required(),
+  categoryEn: Joi.string().required(),
+  categoryAr: Joi.string().required(),
+  descriptionEn: Joi.string().optional(),
+  descriptionAr: Joi.string().optional(),
+  rate: Joi.number().min(0).max(5).optional(),
+  rentPrice: Joi.number().min(0).optional(),
+  sellPrice: Joi.number().min(0).optional(),
+  availableForRent: Joi.boolean().required(),
+  availableForSale: Joi.boolean().required(),
+  rentStock: Joi.number().integer().min(0).required(),
+  saleStock: Joi.number().integer().min(0).required(),
+  images: Joi.array().items(Joi.string().uri()).required(),
+  videos: Joi.array().items(Joi.string().uri()).optional(),
+  qrCode: Joi.string().uri().optional(),
 });
 
 module.exports = { productSchema };
