4ebb872e9d4b55ba4670df3cc94eeb782b0bff08
molhamsheikh <molhamsa49@gmail.com>
2025-08-05 22:37:38 +0300

    remove product id on path

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,20 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const requireRole = require('../middlewares/requireRole');
-const requireLogin = require('../middlewares/requireLogin');
-const validate = require('../middlewares/validateProductInput');
-const { createProductSchema, editProductSchema } = require('../utils/validationSchemas');
-const { addProduct, removeProduct, changeProduct, getProducts, AddToFavorites, AddToCart } = require('../controllers/productController');
-const upload = require('../middlewares/upload');
+const requireRole = require("../middlewares/requireRole");
+const requireLogin = require("../middlewares/requireLogin");
+const validate = require("../middlewares/validateProductInput");
+const {
+  createProductSchema,
+  editProductSchema,
+} = require("../utils/validationSchemas");
+const {
+  addProduct,
+  removeProduct,
+  changeProduct,
+  getProducts,
+  AddToFavorites,
+  AddToCart,
+} = require("../controllers/productController");
+const upload = require("../middlewares/upload");
 
-router.post('/add-product', requireRole('ADMIN'), upload.fields([
-    { name: 'images', maxCount: 100 },
-    { name: 'videos', maxCount: 100 },
-]), validate(createProductSchema), addProduct);
-router.delete('/delete-product/:id', requireRole('ADMIN'), removeProduct);
-router.patch('/edit-product/:id', requireRole('ADMIN'), validate(editProductSchema), changeProduct);
-router.get('/get-products', getProducts);
-router.post('/add-to-favorites/:productId', requireLogin, AddToFavorites);
-router.post('/add-to-cart/:productId', requireLogin, AddToCart);
+router.post(
+  "/add-product",
+  requireRole("ADMIN"),
+  upload.fields([
+    { name: "images", maxCount: 100 },
+    { name: "videos", maxCount: 100 },
+  ]),
+  validate(createProductSchema),
+  addProduct
+);
+router.delete("/delete-product/:id", requireRole("ADMIN"), removeProduct);
+router.patch(
+  "/edit-product/:id",
+  requireRole("ADMIN"),
+  validate(editProductSchema),
+  changeProduct
+);
+router.get("/get-products", getProducts);
+router.post("/add-to-favorites/:productId", requireLogin, AddToFavorites);
+router.post("/add-to-cart", requireLogin, AddToCart);
 
 module.exports = router;
