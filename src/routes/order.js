bf7f5627279a7678f1df17c27ebd8555f2cbc5e5
saleem-shalabi <saleemshalabi90@gmail.com>
2025-09-03 13:49:14 +0300

    Update order.js

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, confirmPayment, setOrderStatus, listAllOrders, getUserOrders, requestExtension, getCurrentUserContracts, setContractStatus, processReturn } = require('../controllers/orderController');
+const { createOrder, confirmPayment, setOrderStatus, listAllOrders, getUserOrders, requestExtension, getCurrentUserContracts, setContractStatus, processReturn, listAllContracts } = require('../controllers/orderController');
 const requireLogin = require('../middlewares/requireLogin');
 const requireRole = require('../middlewares/requireRole');
 const requireRoles = require('../middlewares/requireRoles');
@@ -11,8 +11,9 @@ router.patch('/:orderId/set-status', requireLogin, requireRole('ADMIN'), setOrde
 router.get('/get-orders', requireLogin, requireRoles(['ADMIN', 'ACCOUNTANT']), listAllOrders);
 router.get('/get-user-orders/:userId', requireLogin, getUserOrders);
 router.post('/extend-contract/:contractId', requireLogin, requestExtension);
-router.get('/get-user-contracts', requireLogin, getCurrentUserContracts);
+router.get('/get-user-contracts', requireLogin, requireRole('USER'), getCurrentUserContracts);
 router.patch('/:contractId/update-contract-status', requireLogin, requireRole('ADMIN'), setContractStatus);
 router.post('/:contractId/return-rented', requireLogin, requireRoles(['ADMIN', 'MAINTENANCE']), processReturn);
+router.get('/list-all-contracts', requireLogin, requireRoles(['ADMIN', 'ACCOUNTANT']), listAllContracts);
 
 module.exports = router;
