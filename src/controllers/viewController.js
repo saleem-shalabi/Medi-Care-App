634b725619ddccdf950bdd210f4840d6aa012c9b
saleem-shalabi <saleemshalabi90@gmail.com>
2025-09-05 22:46:15 +0300

    viewEdit

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -1,8 +1,9 @@
 const { getMainViewService } = require("../services/getMainViewService");
 
 async function getMainView(req, res) {
+  const userId = req.user.id;
   try {
-    const result = await getMainViewService();
+    const result = await getMainViewService(userId, req);
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ error: err.message });
